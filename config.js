4bce40d4f6d4a976cfcc41f60a4112c18d98558a
export-ops <untalanmardy@outlook.com>
2026-06-11 17:46:45 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // List of authorized devices (to be filled later)
-const allowedFingerprints = ["FP-1790961635", "FP-1030012208", "FP-1480633938", "FP-1611382775"];
+const allowedFingerprints = ["FP-1790961635", "FP-1030012208", "FP-1480633938", "FP-1611382775", "FP-100764624"];
 
 const allowedEventCode = "128PH";
